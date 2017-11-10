43fbe0d8126102471b6ad5a347a21fb244a906b4
Eric Abell <ericabell@users.noreply.github.com>
2017-11-09 21:34:18 -0600

    Trivial test case passes

--- test_simple_delete.py
+++ test_simple_delete.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 from simple_delete import rm
 
 import os.path
@@ -10,9 +8,12 @@ class RmTestCase(unittest.TestCase):
     tmpfilepath = os.path.join(tempfile.gettempdir(), "tmp-testfile")
 
     def setUp(self):
-        with open(self.tempfilepath, "wb") as f:
+        with open(self.tmpfilepath, "wb") as f:
             f.write("Delete me!")
 
     def test_rm(self):
         rm(self.tmpfilepath)
-        self.assertFalse(os.path.isfile(self.tmpfilepath, "Failed to remove the file.")
+        self.assertFalse(os.path.isfile(self.tmpfilepath), "Failed to remove the file.")
+
+if __name__ == '__main__':
+    unittest.main()
