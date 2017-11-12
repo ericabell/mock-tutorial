a11c76ff950f96100b02c26cf98142a7758377fe
Eric Abell <ericabell@users.noreply.github.com>
2017-11-12 10:43:36 -0600

    Add some notes about how mock is working

--- test_simple_delete.py
+++ test_simple_delete.py
@@ -15,10 +15,17 @@ class RmTestCase(unittest.TestCase):
         with open(self.tmpfilepath, "w") as f:
             f.write("Delete me!")
 
-    @mock.patch('simple_delete.os')
+    @mock.patch('simple_delete.os') # because rm gets called *in* simple_delete.os
     def test_rm(self, mock_os):
         rm("any path")
+        # now we run the check to see that rm was called correctly
+        # note: we don't actually have to do this, since mocking out simple_delete.os
+        # just won't do anything.
         mock_os.remove.assert_called_with("any path")
+
+    def test_rm_real(self):
+        rm(self.tmpfilepath)
+        self.assertFalse(os.path.isfile(self.tmpfilepath), "Failed to remove the file.")
 
 if __name__ == '__main__':
     unittest.main()
