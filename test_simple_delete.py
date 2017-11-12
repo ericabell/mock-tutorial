57202ec4970863e78052c026d8293c6eb399adcd
Eric Abell <ericabell@users.noreply.github.com>
2017-11-12 10:37:42 -0600

    Create test file, but since rm is mocked, file remains after test run

--- test_simple_delete.py
+++ test_simple_delete.py
@@ -1,9 +1,19 @@
 from simple_delete import rm
 
+import os.path
+import tempfile
+
 from unittest import mock
 import unittest
 
 class RmTestCase(unittest.TestCase):
+
+    tmpfilepath = os.path.join(tempfile.gettempdir(), "tmp-testfile")
+    print(tmpfilepath)
+
+    def setUp(self):
+        with open(self.tmpfilepath, "w") as f:
+            f.write("Delete me!")
 
     @mock.patch('simple_delete.os')
     def test_rm(self, mock_os):
