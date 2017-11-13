0436eb49b49cc7853adf0f8238a926b864a138c3
Eric Abell <ericabell@users.noreply.github.com>
2017-11-12 21:03:33 -0600

    Refactor rm to be a service class instead of single method

--- simple_delete.py
+++ simple_delete.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import os
+import os.path
 
-def rm(filename):
-    if os.path.isfile(filename):
-        os.remove(filename)
+class RemovalService(object):
+    """
+    A service for removing objects from the filesystem
+    """
+    def rm(filename):
+        if os.path.isfile(filename):
+            os.remove(filename)
